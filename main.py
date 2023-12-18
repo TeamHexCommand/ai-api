03c35f4e368897ffb02748015e85558c055f131f
Yash Gohel <yashgohel16@gmail.com>
2023-12-18 19:59:20 +0530

    Background remover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, FileResponse
 from PIL import Image
 import requests
 from transformers import BlipProcessor, BlipForConditionalGeneration
@@ -7,6 +7,10 @@ import urllib.request
 import os
 import openai
 from dotenv import load_dotenv
+import cv2
+import numpy as np
+from transparent_background import Remover
+import uuid 
 
 load_dotenv()
 
@@ -17,11 +21,26 @@ processor = BlipProcessor.from_pretrained(model_id)
 openai.api_key = os.getenv("OPENAI")
 model_engine = "text-davinci-003"
 
+remover = Remover()
+
+output_folder = 'output'
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
 def blipCaption(img: str):
     image = Image.open(requests.get(img, stream=True).raw).convert('RGB')
     inputs = processor(image, return_tensors="pt")
     out = model.generate(**inputs)
     return processor.decode(out[0], skip_special_tokens=True)
+
+
+def removeBackground(img: str):
+    image = Image.open(requests.get(img, stream=True).raw).convert('RGB')
+    out = remover.process(image, type='rgba')
+    name = uuid.uuid4()
+    filename = f'{output_folder}/{name}.png'
+    out.save(filename)
+    return name
 
 app = FastAPI()
 
@@ -30,7 +49,7 @@ def read_root():
     return {"msg": "BLIP Running"}
 
 @app.get("/info")
-def read_root():
+def read_info():
     return {"msg": "Team HexCommand"}
 
 @app.get("/blip/caption/{img}")
@@ -49,3 +68,33 @@ def read_item(prompt: str):
         temperature=0.5,
     )
     return {"prompt": prompt, "answer": completion.choices}
+
+
+@app.get("/image/removebg/{img}")
+def read_item(img: str):
+    ans = removeBackground(requests.utils.unquote(img))
+    image_url = get_image_url(ans)
+    if image_url:
+        return {"img": image_url}
+    else:
+        return {"img": uuid, "error": "Image not found"}
+
+
+def get_image_url(uuid: str):
+    output_folder = 'output'
+    filename = f'{output_folder}/{uuid}.png'
+    if os.path.exists(filename):
+        return f'{os.getenv("BASEURL")}/{filename}'
+    else:
+        return None
+
+
+@app.get("/image/{img}")
+def read_image(img: str):
+    uuid = requests.utils.unquote(img)
+    image_url = get_image_url(uuid)
+    
+    if image_url:
+        return FileResponse(image_url, media_type='image/png')
+    else:
+        return {"img": uuid, "error": "Image not found"}
